2ea5d0bf0b6076aba13db72a307378a803c461b0
freedy79 <michael.free@gmx.de>
2026-01-08 13:25:24 +0100

    Update output translations on language changes

--- frontend/app/shared/services/qtf-translation.service.ts
+++ frontend/app/shared/services/qtf-translation.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable, of } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QtfTranslationService {
-  private cache = new Map<string, Observable<string>>();
-
   constructor(private translateService: TranslateService) {}
 
   get(key: string): Observable<string> {
@@ -16,17 +13,6 @@ export class QtfTranslationService {
       return of('');
     }
 
-    const language = this.translateService.currentLang || this.translateService.defaultLang || '';
-    const cacheKey = `${language}::${key}`;
-    const cached = this.cache.get(cacheKey);
-    if (cached) {
-      return cached;
-    }
-
-    const translation$ = this.translateService.get(key).pipe(
-      shareReplay({ bufferSize: 1, refCount: false })
-    );
-    this.cache.set(cacheKey, translation$);
-    return translation$;
+    return this.translateService.stream(key);
   }
 }
