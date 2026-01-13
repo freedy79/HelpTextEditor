e33dd0af34ecdbe9f0dafe3fdc5651e375423638
freedy79 <michael.free@gmx.de>
2026-01-13 12:53:19 +0100

    Ensure karma fixRequestHeaders middleware runs

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -37,6 +37,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
+    middleware: ['fixRequestHeaders'],
     autoWatch: true,
     browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: true,
